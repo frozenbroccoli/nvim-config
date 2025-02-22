3925e14fd94f599c9e7a48afd94393427b355d4a
Souvik Naskar <shouvik0104@gmail.com>
2025-02-23 02:11:27 +0530

    added bracket finisher and fixed typo in remap

--- lua/shouvik/packer.lua
+++ lua/shouvik/packer.lua
@@ -25,6 +25,7 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  use('rstacruz/vim-closer')
 
   use {
 	  'Vonheikemen/lsp-zero.nvim',
